b3896ad4a469132880438be04824a08995e690a8
Pepone <Pepone@MADRE>
2024-03-03 18:26:43 +0100

    Agregar archivos de proyecto.

---
+++ HolaMundo/HolaMundo/BienvenidaPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HolaMundo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BienvenidaPage : ContentPage
+    {
+        public BienvenidaPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Hola!!!","¡¡¡Maricón!!!","Cerrar");
+        }
+    }
+}
---
+++ Pruebas_1/Pruebas_1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Pruebas_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            // Label
+            lbl_num.TextColor = Color.White;
+            lbl_num.Text = "0";
+            lbl_num.AnchorX = 10;
+            lbl_num.AnchorY = 10;
+            lbl_num.
+        }
+
+        private void Button_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
